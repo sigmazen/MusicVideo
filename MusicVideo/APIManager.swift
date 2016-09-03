a8cd8fb747f5f165bb85651bf9cd06af8b5eaf5c
sigmazen <sigmazen@hotmail.com>
2016-09-02 17:41:45 -0700

    Complete Part03

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -21,17 +21,47 @@ class APIManager {
         let task = session.dataTaskWithURL(url) {
             (data, response, error) -> Void in
             
-            //Move resultset to main thread
-            dispatch_async(dispatch_get_main_queue()) {
-                if error != nil {
+            if error != nil {
+                dispatch_async(dispatch_get_main_queue()) {
                     completion(result: (error!.localizedDescription))
-                } else {
-                    completion(result: "NSURLSession successful")
-                    print(data)
+                }
+
+            } else {
+                
+                do {
+                    /*
+                    AllowFragments is used when top level object is not Array or Dictionary
+                    Check the JSON using jsoneditoronline.org
+                        {} is Dictionary    [] is Array
+                    Therefore use AnyObject for Dictionary
+                    */
+                    if let json = try NSJSONSerialization.JSONObjectWithData(data!,
+                        options: .AllowFragments)
+                        as? [String: AnyObject] {
+
+                            print(json)
+                            
+                            let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                            
+                            dispatch_async(dispatch_get_global_queue(priority, 0)) {
+
+                                dispatch_async(dispatch_get_main_queue()) {
+                                    completion(result: "NSJSONSerialization Successful")
+                                }
+
+                            }
+                    }
+                
+                } catch {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        completion(result: "Error in NSJSONSerialization")
+                    }
                 }
             }
-        }
+        // End of NSJSONSerialization
         
+        }
+
         task.resume()
     }
 }
