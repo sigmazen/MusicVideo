54310f37ebe5e4b0e70c36aee3e0c5002d9890e0
sigmazen <sigmazen@hotmail.com>
2016-09-02 17:00:13 -0700

    Lesson 32 Completed

---
+++ MusicVideo/APIManager.swift
@@ -0,0 +1,37 @@
+//
+//  APIManager.swift
+//  MusicVideo
+//
+//  Created by Simon R Mableson on 9/2/16.
+//  Copyright © 2016 Simon R Mableson. All rights reserved.
+//
+
+import Foundation
+
+class APIManager {
+    
+    func loadData(urlString:String, completion: (result:String) -> Void) {
+        
+        let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
+        
+        let session = NSURLSession(configuration: config)
+        
+        let url = NSURL(string: urlString)!
+        
+        let task = session.dataTaskWithURL(url) {
+            (data, response, error) -> Void in
+            
+            //Move resultset to main thread
+            dispatch_async(dispatch_get_main_queue()) {
+                if error != nil {
+                    completion(result: (error!.localizedDescription))
+                } else {
+                    completion(result: "NSURLSession successful")
+                    print(data)
+                }
+            }
+        }
+        
+        task.resume()
+    }
+}
